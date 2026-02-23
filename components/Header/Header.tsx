b9eeb6dde2e5ea3a17c836998fb706734a0e66f1
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-02-23 15:46:31 +0200

    Create Layout

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <section className="container">
-      <h4>Header</h4>
+      <h4>New Header</h4>
     </section>
   );
 }
--- components/Home/Layouts/Layout .tsx
+++ components/Home/Layouts/Layout .tsx
@@ -1,7 +1,52 @@
-export default function Layout() {
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import Head from 'next/head';
+
+const Layout = ({
+  children,
+  title,
+  description,
+}: {
+  children: React.ReactNode;
+  title?: string;
+  description?: string;
+}) => {
   return (
-    <section className="container">
-      <h2>Контент створений для...</h2>
-    </section>
+    <>
+      <Head>
+        <title>{title ? title : 'Traveller App'}</title>
+        <meta
+          name="description"
+          content={description ? description : 'Traveller frontend application'}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="FullStack developers" />
+        <meta property="og:title" content={title ? title : 'Traveller App'} />
+        <meta
+          property="og:description"
+          content={description ? description : 'Traveller frontend application'}
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://img.tsn.ua/cached/853/tsn-15890496c3fba55a55e21f0ca3090d06/thumbs/1200x630/04/c6/e62676060bbb0834d06c603357e5c604.jpeg"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Travelers" />
+      </Head>
+
+      <div className="container">
+        <header>
+          <Header />
+        </header>
+        <main>{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
+    </>
   );
-}
+};
+
+export default Layout;
