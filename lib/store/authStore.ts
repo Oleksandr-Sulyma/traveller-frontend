b7fab495efe7f5cbcb21afc3f1e459c27ca4726f
TatiYas <150725014+TatiYas@users.noreply.github.com>
2026-02-23 17:03:28 +0100

    add. Header (#36)
    * lib/store/authStore.ts
    
    * Update authStore.ts
    
    * Update authStore.ts
    
    * fix conflicts

--- lib/store/authStore.ts
+++ lib/store/authStore.ts
@@ -1,14 +1,12 @@
 import { create } from "zustand";
 import { User } from "@/types/user";
 
-
 interface AuthStore {
   isAuthenticated: boolean;
   user: User | null;
   setUser: (user: User) => void;
   clearIsAuthenticated: () => void;
 }
-
 export const useAuthStore = create<AuthStore>()((set) => ({
   isAuthenticated: false,
   user: null,
