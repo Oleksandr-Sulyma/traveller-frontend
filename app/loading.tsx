0561560a510006fdef7940dca8be82db91c19cc3
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-02-23 11:01:27 +0200

    Simplify Loading component definition

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,5 @@
 import Loader from '@/components/Loader/Loader';
 
-const Loading = () => {
-  return <Loader color="#FFFFFF" size={50} />;
-};
+const Loading = () => <Loader color="#FFFFFF" size={50} />;
 
 export default Loading;
