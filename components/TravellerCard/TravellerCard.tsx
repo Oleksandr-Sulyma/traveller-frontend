b0ca9214363256a87b9ae1ad5777d6f1da40865d
Vadym Samardak <fatalerror14.88@icloud.com>
2026-02-24 16:09:40 +0100

    just save my sketch

---
+++ components/TravellerCard/TravellerCard.tsx
@@ -0,0 +1,33 @@
+import styles from './TravellerCard.module.css';
+import type { User } from '../../types/user';
+
+interface TravelerCardProps extends Pick<User, 'avatarUrl' | 'description' | 'name'> {
+  photo: string;
+  buttonText?: string;
+  onViewProfile?: () => void;
+}
+
+export default function TravelerCard({
+  photo,
+  name,
+  description,
+  buttonText = 'Переглянути профіль',
+  onViewProfile,
+}: TravelerCardProps) {
+  return (
+    <div className={`traveler-card ${styles.traveler_card}`} data-theme="dark">
+      <img src={photo} alt={name} className={styles.traveler_card_photo} />
+      <div className={styles.traveler_card_content}>
+        <h3 className={styles.traveler_card_name}>{name}</h3>
+        <p className={styles.traveler_card_text}>{description}</p>
+
+        <button
+          className={`btn btn-secondary traveler-card-button  ${styles.traveler_card_button}`}
+          onClick={onViewProfile}
+        >
+          {buttonText}
+        </button>
+      </div>
+    </div>
+  );
+}
