442b9fb59d2ce2b1fba2b97968ee1176011493f0
koselevn <koselevn52@gmail.com>
2026-02-24 20:22:48 +0100

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,28 @@ import '@/styles/components/cards.css';
 import './fonts.css';
 
 import Header from '@/components/Header/Header';
-import Footer from '@/components/Footer/Footer';
+// import Footer from '@/components/Footer/Footer';
 import FooterWrapper from '@/components/Footer/FooterWrapper';
+import { BASE_URL, SITE_DESCRIPTION, SITE_NAME, SITE_SMAL_DESCRIPTION } from '@/lib/constants/seo';
 
 export const metadata: Metadata = {
-  title: 'Подорожники',
-  description: 'Мандрівки вашої мрії',
+  title: SITE_SMAL_DESCRIPTION,
+  description: SITE_DESCRIPTION,
+  openGraph: {
+        title: SITE_SMAL_DESCRIPTION,
+        description: SITE_DESCRIPTION,
+        url: BASE_URL,
+        siteName: SITE_SMAL_DESCRIPTION,
+        images: [
+          {
+            url: '../public/images/title-bg.png',
+            width: 1200,
+            height: 630,
+            alt: SITE_NAME,
+          },
+        ],
+        type: 'website',
+      },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
