7d10ac44a53c8eb1b0cbcfb8f34627c9f6ad358c
Vadym Samardak <fatalerror14.88@icloud.com>
2026-02-24 08:03:11 +0100

    used global styles

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import styles from './Footer.module.css';
+import '@/styles/components/links.css';
 import Image from 'next/image';
 import logo from '@/public/favicon.svg';
 
@@ -17,22 +18,42 @@ export default function Footer() {
           </Link>
 
           <div className={styles.social}>
-            <a href="https://www.facebook.com/login" target="_blank" aria-label="Facebook">
+            <a
+              href="https://www.facebook.com/login"
+              target="_blank"
+              aria-label="Facebook"
+              className="link-base link-icon"
+            >
               <svg className={styles.icon} viewBox="0 0 32 32">
                 <use href="/sprites/sprite.svg#icon-Facebook" />
               </svg>
             </a>
-            <a href="https://www.instagram.com/" target="_blank" aria-label="Instagram">
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              aria-label="Instagram"
+              className="link-base link-icon"
+            >
               <svg className={styles.icon} viewBox="0 0 32 32">
                 <use href="/sprites/sprite.svg#icon-Instagram" />
               </svg>
             </a>
-            <a href="https://twitter.com/" target="_blank" aria-label="Twitter">
+            <a
+              href="https://twitter.com/"
+              target="_blank"
+              aria-label="Twitter"
+              className="link-base link-icon"
+            >
               <svg className={styles.icon} viewBox="0 0 32 32">
                 <use href="/sprites/sprite.svg#icon-X" />
               </svg>
             </a>
-            <a href="https://www.youtube.com/" target="_blank" aria-label="YouTube">
+            <a
+              href="https://www.youtube.com/"
+              target="_blank"
+              aria-label="YouTube"
+              className="link-base link-icon"
+            >
               <svg className={styles.icon} viewBox="0 0 32 32">
                 <use href="/sprites/sprite.svg#icon-Youtube" />
               </svg>
@@ -40,9 +61,21 @@ export default function Footer() {
           </div>
 
           <nav className={styles.nav}>
-            <Link href="/">Головна</Link>
-            <Link href="/stories">Історії</Link>
-            <Link href="/travellers">Мандрівники</Link>
+            <Link href="/" className="link-base link-text link-footer-pc link-footer-mobile ">
+              Головна
+            </Link>
+            <Link
+              href="/stories"
+              className="link-base link-text link-footer-pc link-footer-mobile "
+            >
+              Історії
+            </Link>
+            <Link
+              href="/travellers"
+              className="link-base link-text link-footer-pc link-footer-mobile "
+            >
+              Мандрівники
+            </Link>
           </nav>
         </div>
 
