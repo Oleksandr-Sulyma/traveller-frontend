73e4902c0fd47d2de23415516add51030c4a7b1a
Олександр <sulima.ab@gmail.com>
2026-02-25 12:49:42 +0200

    feat:add Hero section

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,17 +1,43 @@
-// import s from './Hero.module.css';
+import { title } from 'process';
+import styles from './Hero.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Hero() {
+
   return (
-    <section>
-      <div className="container">
-        {/* Використовуємо базовий H1 та базовий p */}
-        <h1>Твої пригоди варті того, щоб про них знали</h1>
-        <p>Ділися історіями та знаходь нові маршрути разом із нами.</p>
+    
+    <section className={styles.heroContainer}>
+      <picture>
+        <source
+          media="(min-width: 1440px)"
+          srcSet="/images/hero/desktop@1x.webp 1x, /images/hero/desktop@2x.webp 2x"
+        />
+        <source
+          media="(min-width: 768px)"
+          srcSet="/images/hero/tablet@1x.webp 1x, /images/hero/tablet@2x.webp 2x"
+        />
+        <Image
+          src="/images/hero/mobile@1x.webp"
+          alt="Traveler Background"
+          fill
+          priority
+          quality={85}
+          className={styles['hero-image']}
+        />
+      </picture>
+      <div className={`container ${styles.container}`}>
+       
+       <h1 className={`hero-title ${styles['hero-title']}`}>
+        Відкрийте світ подорожей з нами!
+        </h1>
+        <p  className={`hero-text ${styles['hero-text']}`}>
+          Приєднуйтесь до нашої спільноти мандрівників, де ви зможете ділитися своїми історіями та отримувати натхнення для нових пригод. Відкрийте для себе нові місця та знайдіть однодумців!
+          </p>
         
-        {/* клас для великої кнопки */}
-        <button className="button-primary button-primary-large">
-          Розпочати подорож
-        </button>
+        <Link href="/sign-up" className="btn btn--hero">
+          Доєднатись
+        </Link>
       </div>
     </section>
   );
