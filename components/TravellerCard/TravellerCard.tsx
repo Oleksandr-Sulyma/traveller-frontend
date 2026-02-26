106ee94ec16ba56c494d07bd782ce10930662a6b
Vadym Samardak <fatalerror14.88@icloud.com>
2026-02-26 12:03:09 +0100

    removed unused import from footer, applied minor traveler card cleanup

--- components/TravellerCard/TravellerCard.tsx
+++ components/TravellerCard/TravellerCard.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import type { User } from '../../types/user';
 
 interface TravelerCardProps extends Pick<User, '_id' | 'avatarUrl' | 'description' | 'name'> {
-  avatarUrl: string;
   buttonText?: string;
 }
 
