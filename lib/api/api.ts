a3537482feb71bfca7d6de65485cdfe8f32e0e69
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2026-02-28 20:14:30 +0000

    Auth/pages (#98)
    * finish-sign-up page
    
    * add: sign-in page, fix: layout, fix: change username to name, fix: make next server work properly, fix: RegisterRequest

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
+
 const nextServer = axios.create({
-  baseURL: 'https://traveller-backend-lia1.onrender.com/api',
+  baseURL: baseURL,
   withCredentials: true,
 });
 
