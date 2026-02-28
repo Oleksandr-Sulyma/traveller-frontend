cfd0a74f3c1b1b56fc8e061fd7c5ae904cdbebef
Ocheretko Oleh <117608037+Ocheretko1992@users.noreply.github.com>
2026-03-01 00:32:06 +0200

    Create section Join

--- components/Home/CTA/CTA.tsx
+++ components/Home/CTA/CTA.tsx
@@ -1,7 +1,29 @@
+import Link from 'next/link';
+import css from './CTA.module.css';
+
+let isAuth = false
+
 export default function CTA() {
   return (
     <section className="container">
-      <h2>Приєднуйтесь до нашої спільноти</h2>
+      <div className={css.position}>
+        <div className={css.wrapper}>
+          <h2 className={css.title}>Приєднуйтесь до нашої спільноти</h2>
+          <p className={css.description}>
+            Долучайтеся до мандрівників, які діляться своїми історіями та надихають на нові пригоди.
+          </p>
+
+          {isAuth ? (
+            <Link className="btn btn-primary" style={{ height: '48px' }} href="/profile/saved">
+              Збережені
+            </Link>
+          ) : (
+            <Link className="btn btn-primary" style={{ height: '48px' }} href="/sign-up">
+              Зареєструватися
+            </Link>
+          )}
+        </div>
+      </div>
     </section>
   );
 }
