95ea92e40483df42e31766d075f7b85af976dbdb
Olesia Konashchuk <konaschukl@gmail.com>
2026-03-01 01:02:58 +0100

    add public traveller profile page

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const nextServer = axios.create({
-  baseURL: 'https://traveller-backend-lia1.onrender.com/api',
+  baseURL: 'https://traveller-backend-lia1.onrender.com/',
   withCredentials: true,
 });
 
