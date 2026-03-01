35e57bc6f0bbc907efa23feee46b9b56a856dc31
Olesia Konashchuk <konaschukl@gmail.com>
2026-03-01 07:21:38 +0100

    add public traveller profile page (#104)
    Co-authored-by: Oleksandr Sulyma <sulima.ab@gmail.com>

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
 
 const nextServer = axios.create({
-  baseURL: baseURL,
+  baseURL: 'https://traveller-backend-lia1.onrender.com/',
   withCredentials: true,
 });
 
