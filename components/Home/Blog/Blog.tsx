6e020f7540a21cc25c801b0b15e653b9b5d20917
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-03-01 10:04:37 +0200

    Feat/grid api final (#106)
    * feat: implement grid system and backend data fetching
    
    * feat: finalize grid system, api categories, and responsive CTA component
    
    * fix: force change case for CTA component to Cta
    
    * fix css
    
    * fix: story props type mismatch in traveller page

--- components/Home/Blog/Blog.tsx
+++ components/Home/Blog/Blog.tsx
@@ -6,3 +6,25 @@ export default function Blog() {
     </section>
   );
 }
+
+// 'use client';
+
+// import css from './Blog.module.css';
+// import StoryCard from '@/components//StoryCard/StoryCard';
+// import { Story } from '@/types/story'; 
+// import Link from 'next/link';
+// import 
+
+// interface Props {
+//   stories: Story[];
+// }
+
+// export default function Blog({stories}: Props) {
+//   return (
+//     <section className={'container'}>
+// <h2 className= {css.title_blog}>Популярні історії</h2>
+
+
+//     </section>
+//   )
+// }
