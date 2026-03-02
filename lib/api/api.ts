0ff663eee07179edd958bef8b001d4ec0f7287b0
Oleksandr Sulyma <sulima.ab@gmail.com>
2026-03-02 22:56:03 +0200

    Uncomment axios import and configuration
    Uncomment axios import and nextServer configuration.

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,11 +1,4 @@
-// import axios from 'axios';
-
-// const baseURL = process.env.NEXT_PUBLIC_API_URL + '/api';
-
-// const nextServer = axios.create({
-//   baseURL,
-//   withCredentials: true,
-// });
+import axios from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL +'/api' ;
 
