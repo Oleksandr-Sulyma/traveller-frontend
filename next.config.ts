040cde4a14b13ff814d26361bcf42518737c1494
Vadym_Samardak <fatalerror14.88@icloud.com>
2026-03-04 18:57:18 +0100

    refactor: align User and TravelerCard types with backend (#144)
    * refactor: align User and TravelerCard types with backend
    
    - Updated User interface to match recent backend changes
    - Synchronized TravelerCard extension
    - Fixed legacy code usage to prevent deployment failure
    
    * fix: replace hardcoded localhost:5000 with env variable in next.config.ts
    feat: add POST route for saving story to favorites
    
    ---------
    
    Co-authored-by: Oleksandr Sulyma <sulima.ab@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const backendUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*', 
+        destination: `${backendUrl}/:path*`, 
       },
 
     ];
