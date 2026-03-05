91430110c0ca0ebb9899f8d07d3b29d7e0a1aa5f
Vitalii Savchuk <vetalsavchuk23@gmail.com>
2026-03-05 15:23:21 +0000

    create stories page and change url to page (#148)

--- components/Profile/OwnStories/OwnStories.tsx
+++ components/Profile/OwnStories/OwnStories.tsx
@@ -1,32 +1,36 @@
-import StoryCard from "@/components/StoryCard/StoryCard";
-import { SavedStoriesProps } from "@/types/profile";
-import Link from "next/link";
-import css from "./OwnStories.module.css"
+import StoryCard from '@/components/StoryCard/StoryCard';
+import { SavedStoriesProps } from '@/types/profile';
+import Link from 'next/link';
+import css from './OwnStories.module.css';
 
 const OwnStories = ({ stories, page }: SavedStoriesProps) => {
-    return (
-        <section>
-            <div className="container">
-                <div className={css.wrapDiv}>
-                {stories.length != 0 ?
-                    stories.map((story) => (
-                        <StoryCard key={story.id} {...story} />
-                    )) : page === "profile" ?
-
-                    <div className={css.div}>
-                        <h2 className={css.title}>Ви ще нічого не публікували, поділіться своєю першою історією!</h2>
-                        <Link href="/stories/create" className={css.btn}>Опублікувати історію</Link>
-                    </div>
-                        : 
-                    <div className={css.div}>
-                        <h2 className={css.title}>Цей користувач ще не публікував історій</h2>
-                        <Link href="/stories" className={css.btn}>Назад до історій</Link>
-                    </div>
-                    }
-                </div>
+  return (
+    <section>
+      <div className="container">
+        <div className={css.wrapDiv}>
+          {stories.length != 0 ? (
+            stories.map(story => <StoryCard key={story.id} {...story} />)
+          ) : page === 'profile' ? (
+            <div className={css.div}>
+              <h2 className={css.title}>
+                Ви ще нічого не публікували, поділіться своєю першою історією!
+              </h2>
+              <Link href="/stories/create" className={css.btn}>
+                Опублікувати історію
+              </Link>
             </div>
-        </section>
-    )
-}
+          ) : (
+            <div className={css.div}>
+              <h2 className={css.title}>Цей користувач ще не публікував історій</h2>
+              <Link href="/stories/filter/all" className={css.btn}>
+                Назад до історій
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default OwnStories;
